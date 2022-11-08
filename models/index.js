6a180f266ee6e77e4e80bdb093617f96aa596fcb
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-08 16:46:00 -0600

    created model index and established model relationships

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require('./Users');
+const Comment = require('./Comment');
+const College = require('./Colleges');
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+College.hasMany(Comment, {
+    foreignKey: 'college_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(College, {
+    foreignKey: 'college_id'
+});
+
+User.belongsToMany(College, {through: 'UserCollege'});
+College.belongsToMany(User, {through: 'UserCollege'});
+
+module.exports = {
+    User,
+    College,
+    Comment,
+    UserCollege
+};
