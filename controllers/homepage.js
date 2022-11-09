dd5faa13a68ab868b350e55f1c778610f59b3e6b
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-09 14:01:31 -0600

    homepage root and login routes complete

---
+++ controllers/homepage.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { User, College, Comment } = require('../models');
+const verifyLogin = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage', {
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if(req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
