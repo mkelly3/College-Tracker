1c1361ed04d561cea475dc8ff1e7bf60c43a5c88
Peter Kim <peter.kim@uconn.edu>
2022-11-10 17:30:52 -0500

    axios now calls all pages within the api query

--- seeds/college-api.js
+++ seeds/college-api.js
@@ -7,15 +7,15 @@ let numPages = 0;
 // TODO: hide api key
 // Currently querying for schools of at least 5,000 students
 let url =
-	"https://api.data.gov/ed/collegescorecard/v1/schools.json?2020.student.size__range=5000..&_fields=school.zip,school.city,school.state,school.school_url,2020.student.size,2020.student.demographics.men,2020.student.demographics.women,2020.cost.tuition.in_state,2020.cost.tuition.out_of_state,2020.cost.roomboard.oncampus,2020.cost.roomboard.offcampus,2020.cost.avg_net_price.public,2020.cost.avg_net_price.private,2020.completion.consumer_rate,2020.admissions.admission_rate.overall,2020.academics.program_available.assoc,2020.academics.program_available.bachelors&per_page=100&page=" +
-	pageNumber +
-	"&api_key=dra3zfoeWMdlOAkY6G0ceNBrdM1sYscwYQbnINMA";
-let starterurl =
-	"https://api.data.gov/ed/collegescorecard/v1/schools.json?2020.student.size__range=5000..&_fields=school.zip,school.city,school.state,school.school_url,2020.student.size,2020.student.demographics.men,2020.student.demographics.women,2020.cost.tuition.in_state,2020.cost.tuition.out_of_state,2020.cost.roomboard.oncampus,2020.cost.roomboard.offcampus,2020.cost.avg_net_price.public,2020.cost.avg_net_price.private,2020.completion.consumer_rate,2020.admissions.admission_rate.overall,2020.academics.program_available.assoc,2020.academics.program_available.bachelors&per_page=100&page=0&api_key=dra3zfoeWMdlOAkY6G0ceNBrdM1sYscwYQbnINMA";
-const getColleges = (currentPageNumber) => {
-	pageNumber = currentPageNumber;
-	return axios.get(url).then((data) => {
-		console.log(url);
+	"https://api.data.gov/ed/collegescorecard/v1/schools.json?2020.student.size__range=5000..&_fields=school.name,school.zip,school.city,school.state,school.school_url,2020.student.size,2020.student.demographics.men,2020.student.demographics.women,2020.cost.tuition.in_state,2020.cost.tuition.out_of_state,2020.cost.roomboard.oncampus,2020.cost.roomboard.offcampus,2020.cost.avg_net_price.public,2020.cost.avg_net_price.private,2020.completion.consumer_rate,2020.admissions.admission_rate.overall,2020.academics.program_available.assoc,2020.academics.program_available.bachelors&page=0&api_key=dra3zfoeWMdlOAkY6G0ceNBrdM1sYscwYQbnINMA";
+
+const getColleges = (pageNumber) => {
+	let query =
+		"https://api.data.gov/ed/collegescorecard/v1/schools.json?2020.student.size__range=5000..&_fields=school.name,school.zip,school.city,school.state,school.school_url,2020.student.size,2020.student.demographics.men,2020.student.demographics.women,2020.cost.tuition.in_state,2020.cost.tuition.out_of_state,2020.cost.roomboard.oncampus,2020.cost.roomboard.offcampus,2020.cost.avg_net_price.public,2020.cost.avg_net_price.private,2020.completion.consumer_rate,2020.admissions.admission_rate.overall,2020.academics.program_available.assoc,2020.academics.program_available.bachelors&per_page=100&page=" +
+		pageNumber +
+		"&api_key=dra3zfoeWMdlOAkY6G0ceNBrdM1sYscwYQbnINMA";
+	return axios.get(query).then((data) => {
+		console.log(query);
 		// returns an array of only college results data
 		// instead of console logging, push it to collegeResultsData
 		if (data.data.results.length > 0) {
@@ -31,7 +31,7 @@ const getColleges = (currentPageNumber) => {
 };
 
 axios
-	.get(starterurl)
+	.get(url)
 	.then((data) => {
 		const totalData = data.data.metadata.total;
 		numPages = Math.ceil(totalData / 100); // will return the number of pages within the api query
