6332230f80cdf15230ae5b6ad995e0f6b3f70525
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-11 22:28:29 -0600

    added login route to userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -14,3 +14,22 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({where: {username: req.body.username }});
+        const userPassword = userData.checkPassword(req.body.password);
+
+        if (!userData || !userPassword) {
+            res.status(400).json({message: 'Incorrect email or password. Please try again.'});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
