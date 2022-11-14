4b8a01b34f505ed6e9121453c0cac71d592bf8ab
Peter Kim <peter.kim@uconn.edu>
2022-11-14 13:54:24 -0500

    updated college model with api data

--- models/Colleges.js
+++ models/Colleges.js
@@ -1,38 +1,78 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class College extends Model {}
 College.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        size: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        url: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'college'
-    }
-)
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+			allowNull: false,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		// instate: {
+		//     type: DataTypes.INTEGER,
+		//     allowNull: false,
+		// }
+		// outofstate: {
+		//     type: DataTypes.INTEGER,
+		//     allowNull: false,
+		// },
+		// oncampus: {
+		// 	type: DataTypes.INTEGER,
+		// 	allowNull: false,
+		// },
+		// offcampus: {
+		// 	type: DataTypes.INTEGER,
+		// 	allowNull: false,
+		// },
+		size: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		url: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		// location: {
+		// 	type: DataTypes.STRING,
+		// 	allowNull: false,
+		// },
+		// associates: {
+		// 	type: DataTypes.BOOLEAN,
+		// 	allowNull: false,
+		// },
+		// bachelors: {
+		// 	type: DataTypes.BOOLEAN,
+		// 	allowNull: false,
+		// },
+		// admission: {
+		// 	type: DataTypes.DECIMAL(4, 2),
+		// 	allowNull: false,
+		// },
+		// Male_Students: {
+		// 	type: DataTypes.DECIMAL(4, 2),
+		// 	allowNull: false,
+		// },
+		// Female_Students: {
+		// 	type: DataTypes.DECIMAL(4, 2),
+		// 	allowNull: false,
+		// },
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "college",
+	}
+);
 
 module.exports = College;
