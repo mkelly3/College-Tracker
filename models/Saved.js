4b1c2db12f60f78487edae4308ba160bece823a6
ronzhao96 <ronald96919@yahoo.com>
2022-11-14 05:50:59 -0800

    finished initial model for saved.js

---
+++ models/Saved.js
@@ -0,0 +1,28 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Saved extends Model {}
+Saved.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'saved'
+    }
+)
