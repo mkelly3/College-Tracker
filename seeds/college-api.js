2602b526d5d2e7eaab1bf05d249dff1968f6327b
Peter Kim <peter.kim@uconn.edu>
2022-11-14 13:08:32 -0500

    cleaned up comments

--- seeds/college-api.js
+++ seeds/college-api.js
@@ -5,7 +5,7 @@ const collegeResultsData = [];
 let pageNumber;
 let numPages = 0;
 
-// base url to find number of pages in the api query
+// base url to find number of pages in the api query (includes metadata)
 let url =
 	"https://api.data.gov/ed/collegescorecard/v1/schools.json?2020.student.size__range=5000..&_fields=school.name,school.zip,school.city,school.state,school.school_url,2020.student.size,2020.student.demographics.men,2020.student.demographics.women,2020.cost.tuition.in_state,2020.cost.tuition.out_of_state,2020.cost.roomboard.oncampus,2020.cost.roomboard.offcampus,2020.cost.avg_net_price.public,2020.cost.avg_net_price.private,2020.completion.consumer_rate,2020.admissions.admission_rate.overall,2020.academics.program_available.assoc,2020.academics.program_available.bachelors&page=0&api_key=dra3zfoeWMdlOAkY6G0ceNBrdM1sYscwYQbnINMA";
 
