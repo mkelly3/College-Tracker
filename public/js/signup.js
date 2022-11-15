21f464e157907d6515e038d02912bec1d39552a2
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-15 11:48:07 -0600

    Revert "fixed login issue"
    This reverts commit a8fa1697a0caba764cdd2a3781d85bcb2514d5d7.

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,7 +6,7 @@ async function signupFormHandler(event) {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (username && password) {
-        const response = await fetch('/api/user/signup', {
+        const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({
                 username,
--- seeds/comment-seed.js
+++ seeds/comment-seed.js
@@ -3,6 +3,19 @@ const sequelize = require('../config/connection');
 
 const commentData = [
     {
+      comment_text: "Great Job!",
+      user_id: 1,
+      post_id: 1  
+    },
+    {
+        comment_text: "Awesome",
+        user_id: 2,
+        post_id: 2 
+    },
+    {
+        comment_text: "I disagree with your analysis",
+        user_id: 3,
+        post_id: 3
     }
 
 ];
