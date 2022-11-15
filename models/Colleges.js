778812ffe2f3a663ed78fd88a8e997edc98e0183
Morgan Kelly <mkelly5380@gmail.com>
2022-11-15 13:47:03 -0600

    added all the homepages back

--- models/Colleges.js
+++ models/Colleges.js
@@ -1,78 +1,74 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+// module.exports = Post;
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+// create our Post model
 class College extends Model {}
-College.init(
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			primaryKey: true,
-			allowNull: false,
-		},
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		price: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-		// instate: {
-		//     type: DataTypes.INTEGER,
-		//     allowNull: false,
-		// }
-		// outofstate: {
-		//     type: DataTypes.INTEGER,
-		//     allowNull: false,
-		// },
-		// oncampus: {
-		// 	type: DataTypes.INTEGER,
-		// 	allowNull: false,
-		// },
-		// offcampus: {
-		// 	type: DataTypes.INTEGER,
-		// 	allowNull: false,
-		// },
-		size: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-		url: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		// location: {
-		// 	type: DataTypes.STRING,
-		// 	allowNull: false,
-		// },
-		// associates: {
-		// 	type: DataTypes.BOOLEAN,
-		// 	allowNull: false,
-		// },
-		// bachelors: {
-		// 	type: DataTypes.BOOLEAN,
-		// 	allowNull: false,
-		// },
-		// admission: {
-		// 	type: DataTypes.DECIMAL(4, 2),
-		// 	allowNull: false,
-		// },
-		// Male_Students: {
-		// 	type: DataTypes.DECIMAL(4, 2),
-		// 	allowNull: false,
-		// },
-		// Female_Students: {
-		// 	type: DataTypes.DECIMAL(4, 2),
-		// 	allowNull: false,
-		// },
-	},
-	{
-		sequelize,
-		freezeTableName: true,
-		underscored: true,
-		modelName: "college",
-	}
-);
 
-module.exports = College;
+// create fields/columns for Post model
+College.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Instate_Tuition: {
+        type: DataTypes.INTEGER,
+      },
+      Out_Of_State_Tuition: {
+        type: DataTypes.INTEGER,
+      },
+      On_Campus: {
+        type: DataTypes.INTEGER,
+      },
+      Off_Campus: {
+        type: DataTypes.INTEGER,
+      },
+      size: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      associates: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      bachelors: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      Admission_Rate: {
+        type: DataTypes.DECIMAL(4, 2),
+      },
+      Male_Students: {
+        type: DataTypes.DECIMAL(4, 2),
+      },
+      Female_Students: {
+        type: DataTypes.DECIMAL(4, 2),
+      },
+      School_Type: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'college'
+    }
+  );
+  
+  module.exports = College;
