692ed668ecda149b60f67f88f4b4d664add8b16b
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-14 21:43:05 -0600

    bug fixes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homepage.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const dashboardRoutes = require('./dashboard.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
